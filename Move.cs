d3cd0f71b5d18828a23478de41d6236d1431dcb0
joe kuka <joekuka@gmail.com>
2021-04-15 13:27:23 -1000

    Creating a target List

---
+++ Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InterviewTesting
+{
+    public class Move : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Renderer _objRend;
+        [SerializeField]
+        private float _speed = 1.0f;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            TargetDetection.onTargetExit += TargetExit;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+
+        private void TargetExit(GameObject obj)
+        {
+            if (gameObject == obj)
+            {
+                _objRend.material.color = Color.green;
+            }
+        }
+
+        private void OnDisable()
+        {
+            TargetDetection.onTargetExit += TargetExit;
+        }
+    }
+}
+
---
+++ TargetDetection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace InterviewTesting
+{
+    public class TargetDetection : MonoBehaviour
+    {
+        public static event Action<GameObject> onTargetExit;
+
+        [SerializeField]
+        private List<GameObject> _activeTargets = new List<GameObject>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _activeTargets.Add(other.gameObject);
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            _activeTargets.Remove(other.gameObject);
+            if (onTargetExit != null)
+            {
+                onTargetExit(other.gameObject);
+            }
+        }
+    }
+
+
+}
+
